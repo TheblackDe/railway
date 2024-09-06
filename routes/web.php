cf6e73c919715bacca8c07712e5fc42761f6833c
TheblackDe <edgardovelasquez.dev@gmail.com>
2024-09-06 13:30:25 -0500

    third

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/noticias', function () {
 
     return view('noticias.index');
 })->name('noticias.index');
